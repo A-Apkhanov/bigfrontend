c083702c699312a2cbfbae9e9eccb86283af8b22
A-Apkhanov <apkhanov.a.s@gmail.com>
2023-01-09 23:31:41 +0600

    implement curry()

---
+++ curry.js
@@ -0,0 +1,22 @@
+const join = (a, b, c) => {
+  return `${a}_${b}_${c}`
+}
+
+function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    } else {
+      return curried.bind(this, ...args);
+    }
+  }
+}
+
+const curriedJoin = curry(join)
+
+curriedJoin(1, 2, 3) // '1_2_3'
+
+curriedJoin(1)(2, 3) // '1_2_3'
+
+curriedJoin(1, 2)(3) // '1_2_3'
+
